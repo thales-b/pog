00780ccbef6ad47814dff4fc283aaefad50e216d
thales-b <thalesbouraoui@gmail.com>
2024-05-06 22:17:22 +0200

    older files finder

--- findgo/findgo.go
+++ findgo/findgo.go
@@ -3,11 +3,26 @@ package findgo
 import (
 	"io/fs"
 	"path/filepath"
+	"time"
 )
 
 func Files(fsys fs.FS) (paths []string) {
 	fs.WalkDir(fsys, ".", func(p string, d fs.DirEntry, err error) error {
 		if filepath.Ext(p) == ".go" {
+			paths = append(paths, p)
+		}
+		return nil
+	})
+	return paths
+}
+
+func OlderFiles(fsys fs.FS, duration time.Duration) (paths []string) {
+	fs.WalkDir(fsys, ".", func(p string, d fs.DirEntry, err error) error {
+		info, err := fs.Stat(fsys, p)
+		if err != nil {
+			return err
+		}
+		if time.Since(info.ModTime()) > duration {
 			paths = append(paths, p)
 		}
 		return nil
--- findgo/findgo_test.go
+++ findgo/findgo_test.go
@@ -3,9 +3,11 @@ package findgo_test
 import (
 	"archive/zip"
 	"findgo"
+	"io/fs"
 	"os"
 	"testing"
 	"testing/fstest"
+	"time"
 
 	"github.com/google/go-cmp/cmp"
 )
@@ -44,6 +46,25 @@ func TestFilesCorrectlyListsFilesInZIPArchive(t *testing.T) {
 	got := findgo.Files(fsys)
 	if !cmp.Equal(want, got) {
 		t.Error(cmp.Diff(want, got))
+	}
+}
+
+func TestOlderFiles(t *testing.T) {
+	t.Parallel()
+	fsys := fstest.MapFS{
+		"file1.txt": {Mode: fs.ModeAppend, ModTime: time.Now().Add(-30 * 24 * time.Hour)}, "file2.txt": {Mode: fs.ModeAppend, ModTime: time.Now().Add(-10 * 24 * time.Hour)},
+		"subfolder/file3.txt":  {Mode: fs.ModeAppend, ModTime: time.Now().Add(-50 * 24 * time.Hour)},
+		"subfolder/file4.txt":  {Mode: fs.ModeAppend, ModTime: time.Now().Add(-5 * 24 * time.Hour)},
+		"subfolder2/file5.txt": {Mode: fs.ModeAppend, ModTime: time.Now().Add(-35 * 24 * time.Hour)},
+	}
+	want := []string{
+		"file1.txt",
+		"subfolder/file3.txt",
+		"subfolder2/file5.txt",
+	}
+	duration := 30 * 24 * time.Hour
+	got := findgo.OlderFiles(fsys, duration)
+	if !cmp.Equal(want, got) {
 	}
 }
 
