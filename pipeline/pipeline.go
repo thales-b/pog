0efce1af6e4f3a7280fec03ef9e4f750ee4770c8
thales-b <thalesbouraoui@gmail.com>
2024-05-09 00:34:39 +0200

    pipeline

---
+++ pipeline/pipeline.go
@@ -0,0 +1,77 @@
+package pipeline
+
+import (
+	"io"
+	"os"
+	"strings"
+)
+
+type Pipeline struct {
+	Output io.Writer
+	Error  error
+	Reader io.Reader
+}
+
+func New() *Pipeline {
+	return &Pipeline{
+		Output: os.Stdout,
+	}
+}
+
+func FromString(s string) *Pipeline {
+	p := New()
+	p.Reader = strings.NewReader(s)
+	return p
+}
+
+func (p *Pipeline) Stdout() {
+	if p.Error != nil {
+		return
+	}
+	io.Copy(p.Output, p.Reader)
+}
+
+func FromFile(pathname string) *Pipeline {
+	f, err := os.Open(pathname)
+	if err != nil {
+		return &Pipeline{Error: err}
+	}
+	p := New()
+	p.Reader = f
+	return p
+}
+
+func (p *Pipeline) String() (string, error) {
+	if p.Error != nil {
+		return "", p.Error
+	}
+	data, err := io.ReadAll(p.Reader)
+	if err != nil {
+		return "", err
+	}
+	return string(data), nil
+}
+
+func (p *Pipeline) Column(col int) *Pipeline {
+	if p.Error != nil {
+		p.Reader = strings.NewReader("")
+		return p
+	}
+	if col < 1 {
+		p.Error = fmt.Errorf("bad column %d: must be positive",
+			col)
+		return p
+	}
+	result := new(bytes.Buffer)
+	input := bufio.NewScanner(p.Reader)
+	for input.Scan() {
+		fields := strings.Fields(input.Text())
+		if len(fields) < col {
+			continue
+		}
+		fmt.Fprintln(result, fields[col-1])
+	}
+	return &Pipeline{
+		Reader: result,
+	}
+}
---
+++ visitors/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"sort"
+	"strings"
+)
+
+func main() {
+	f, err := os.Open("log.txt")
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+	defer f.Close()
+	input := bufio.NewScanner(f)
+	uniques := map[string]int{}
+	for input.Scan() {
+		fields := strings.Fields(input.Text())
+		if len(fields) > 0 {
+			uniques[fields[0]]++
+		}
+	}
+	type freq struct {
+		addr  string
+		count int
+	}
+	freqs := make([]freq, 0, len(uniques))
+	for addr, count := range uniques {
+		freqs = append(freqs, freq{addr, count})
+	}
+	sort.Slice(freqs, func(i, j int) bool {
+		return freqs[i].count > freqs[j].count
+	})
+	fmt.Printf("%-16s%s\n", "Address", "Requests")
+	for i, freq := range freqs {
+		if i > 9 {
+			break
+		}
+		fmt.Printf("%-16s%d\n", freq.addr, freq.count)
+	}
+}
